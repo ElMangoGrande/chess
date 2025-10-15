ba3c823475a7e22348098aebeaf74bf4197854ea
Hyrum Mangum <hyrummangum@gmail.com>
2025-10-15 16:56:10 -0600

    Wrote MemoryAuthDAO.java

--- server/src/main/java/dataaccess/AuthDao.java
+++ server/src/main/java/dataaccess/AuthDao.java
@@ -7,6 +7,6 @@ import javax.xml.crypto.Data;
 public interface AuthDao {
     void createAuth(AuthData auth) throws DataAccessException;
     AuthData getAuth(String authToken) throws DataAccessException;
-    void deleteAuth(String authToken) throws DataAccessException;
+    void deleteAuth(AuthData auth) throws DataAccessException;
 
 }
---
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -0,0 +1,35 @@
+package dataaccess;
+
+import model.AuthData;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class MemoryAuthDAO implements AuthDao{
+    private static final Set<AuthData> AUTH_DATA= new HashSet<>();
+
+    @Override
+    public void createAuth(AuthData auth) throws DataAccessException{
+        if(AUTH_DATA.contains(auth)){
+            throw new DataAccessException("Error: AuthToken exists already");
+        }else{
+            AUTH_DATA.add(auth);
+        }
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        for(AuthData auth : AUTH_DATA){
+            if(auth.authToken().equals(authToken)){
+                return auth;
+            }
+        }
+        throw new DataAccessException("Error: No authToken Found");
+    }
+
+    @Override
+    public void deleteAuth(AuthData auth){
+        AUTH_DATA.remove(auth);
+    }
+}
