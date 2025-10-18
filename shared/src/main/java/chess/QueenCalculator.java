b9cfe10b0ada75bd0c20c8ef3770c2eceacfd9eb
Hyrum Mangum <hyrummangum@gmail.com>
2025-10-17 21:53:08 -0600

    refactored queen

--- shared/src/main/java/chess/QueenCalculator.java
+++ shared/src/main/java/chess/QueenCalculator.java
@@ -15,141 +15,19 @@ public class QueenCalculator {
     }
 
     public List<ChessMove> calculateMoves(){
-        int startRow = this.startPosition.getRow();
-        int startCol = this.startPosition.getColumn();
+        // Get all diagonal moves from BishopCalculator
+        BishopCalculator bishopCalc = new BishopCalculator(board, startPosition);
+        bishopCalc.calculateMoves();
+        List<ChessMove> bishopMoves = bishopCalc.getPossibleMoves();
 
-        //upper right Diagonal path
-        int row = startRow + 1;
-        int col = startCol + 1;
-        while(row < 9 && col < 9){
-            ChessPosition newPosition = new ChessPosition(row,col);
-            ChessPiece target = board.getPiece(newPosition);
-            if (target == null) {
-                possibleMoves.add(new ChessMove(startPosition, newPosition, null));
-            } else if (target.getTeamColor() != board.getPiece(startPosition).getTeamColor()) {
-                possibleMoves.add(new ChessMove(startPosition, newPosition, null));
-                break;
-            } else {
-                break;
-            }
-            row++;
-            col++;
-        }
-        //upper left diagonal path
-        row = startRow + 1;
-        col = startCol - 1;
-        while(row < 9 && col > 0){
-            ChessPosition newPosition = new ChessPosition(row,col);
-            ChessPiece target = board.getPiece(newPosition);
-            if (target == null) {
-                possibleMoves.add(new ChessMove(startPosition, newPosition, null));
-            } else if (target.getTeamColor() != board.getPiece(startPosition).getTeamColor()) {
-                possibleMoves.add(new ChessMove(startPosition, newPosition, null));
-                break;
-            } else {
-                break;
-            }
-            row++;
-            col--;
-        }
-        //lower right diagonal path
-        row = startRow - 1;
-        col = startCol + 1;
-        while(row > 0 && col < 9){
-            ChessPosition newPosition = new ChessPosition(row,col);
-            ChessPiece target = board.getPiece(newPosition);
-            if (target == null) {
-                possibleMoves.add(new ChessMove(startPosition, newPosition, null));
-            } else if (target.getTeamColor() != board.getPiece(startPosition).getTeamColor()) {
-                possibleMoves.add(new ChessMove(startPosition, newPosition, null));
-                break;
-            } else {
-                break;
-            }
-            row--;
-            col++;
-        }
-        //lower left diagonal path
-        row = startRow - 1;
-        col = startCol - 1;
-        while(row > 0 && col > 0){
-            ChessPosition newPosition = new ChessPosition(row,col);
-            ChessPiece target = board.getPiece(newPosition);
-            if (target == null) {
-                possibleMoves.add(new ChessMove(startPosition, newPosition, null));
-            } else if (target.getTeamColor() != board.getPiece(startPosition).getTeamColor()) {
-                possibleMoves.add(new ChessMove(startPosition, newPosition, null));
-                break;
-            } else {
-                break;
-            }
-            row--;
-            col--;
-        }
 
-        //up path
-        row = startRow + 1;
-        col = startCol;
-        while(row < 9){
-            ChessPosition newPosition = new ChessPosition(row,col);
-            ChessPiece target = board.getPiece(newPosition);
-            if (target == null) {
-                possibleMoves.add(new ChessMove(startPosition, newPosition, null));
-            } else if (target.getTeamColor() != board.getPiece(startPosition).getTeamColor()) {
-                possibleMoves.add(new ChessMove(startPosition, newPosition, null));
-                break;
-            } else {
-                break;
-            }
-            row++;
-        }
-        //down path
-        row = startRow - 1;
-        while(row > 0){
-            ChessPosition newPosition = new ChessPosition(row,col);
-            ChessPiece target = board.getPiece(newPosition);
-            if (target == null) {
-                possibleMoves.add(new ChessMove(startPosition, newPosition, null));
-            } else if (target.getTeamColor() != board.getPiece(startPosition).getTeamColor()) {
-                possibleMoves.add(new ChessMove(startPosition, newPosition, null));
-                break;
-            } else {
-                break;
-            }
-            row--;
-        }
-        //left path
-        row = startRow;
-        col = startCol -1 ;
-        while(col > 0){
-            ChessPosition newPosition = new ChessPosition(row,col);
-            ChessPiece target = board.getPiece(newPosition);
-            if (target == null) {
-                possibleMoves.add(new ChessMove(startPosition, newPosition, null));
-            } else if (target.getTeamColor() != board.getPiece(startPosition).getTeamColor()) {
-                possibleMoves.add(new ChessMove(startPosition, newPosition, null));
-                break;
-            } else {
-                break;
-            }
-            col--;
-        }
-        //right path
-        row = startRow;
-        col = startCol +1 ;
-        while(col < 9){
-            ChessPosition newPosition = new ChessPosition(row,col);
-            ChessPiece target = board.getPiece(newPosition);
-            if (target == null) {
-                possibleMoves.add(new ChessMove(startPosition, newPosition, null));
-            } else if (target.getTeamColor() != board.getPiece(startPosition).getTeamColor()) {
-                possibleMoves.add(new ChessMove(startPosition, newPosition, null));
-                break;
-            } else {
-                break;
-            }
-            col++;
-        }
+        // Get all straight moves from RookCalculator
+        RookCalculator rookCalc = new RookCalculator(board, startPosition);
+        List<ChessMove> rookMoves = rookCalc.calculateMoves();
+
+        // Combine both
+        possibleMoves.addAll(bishopMoves);
+        possibleMoves.addAll(rookMoves);
 
         return possibleMoves;
     }
