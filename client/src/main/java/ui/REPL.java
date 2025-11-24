25c7af7ee29e98a919cf2022dd3fe9bc60f79042
Hyrum Mangum <hyrummangum@gmail.com>
2025-11-24 09:04:38 -0700

    Wrote the WebsocketFacade and added printMessage to the REPL loop

--- client/src/main/java/ui/REPL.java
+++ client/src/main/java/ui/REPL.java
@@ -113,4 +113,9 @@ public class REPL {
 
         }
     }
+
+    public void printMessage(String message) {
+        System.out.println(message);
+    }
+
 }
---
+++ client/src/main/java/websocket/WebsocketFacade.java
@@ -0,0 +1,78 @@
+package websocket;
+
+import chess.ChessMove;
+import com.google.gson.Gson;
+import jakarta.websocket.*;
+import serverhandling.ResponseException;
+import ui.REPL;
+import websocket.commands.JoinCommand;
+import websocket.commands.MakeMoveCommand;
+import websocket.commands.UserGameCommand;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class WebsocketFacade extends Endpoint {
+
+    private Session sesh;
+    private REPL repl;
+
+    public WebsocketFacade(REPL repl, String url) throws Exception {
+        this.repl = repl;
+        URI uri = new URI(url.replace("http","ws"));
+        WebSocketContainer contain = ContainerProvider.getWebSocketContainer();
+        contain.connectToServer(this,uri);
+
+    }
+
+    @Override
+    public void onOpen(Session session, EndpointConfig endpointConfig) {
+        sesh = session;
+        session.addMessageHandler(new MessageHandler.Whole<String>() {
+            @Override
+            public void onMessage(String message){
+                repl.printMessage(message);
+            }
+        });
+    }
+
+
+
+    public void connect(String authToken, int gameID, String color) throws ResponseException {
+        try{
+            JoinCommand joinCommand = new JoinCommand(UserGameCommand.CommandType.CONNECT,authToken,gameID,color);
+            sesh.getBasicRemote().sendText(new Gson().toJson(joinCommand));
+        } catch (IOException e) {
+            throw new ResponseException(e.getMessage());
+        }
+    }
+
+    public void makeMove(String authToken, int gameID, ChessMove move) throws ResponseException {
+        try{
+            MakeMoveCommand moveCommand = new MakeMoveCommand(UserGameCommand.CommandType.MAKE_MOVE,authToken,gameID,move);
+            sesh.getBasicRemote().sendText(new Gson().toJson(moveCommand));
+        } catch (IOException e) {
+            throw new ResponseException(e.getMessage());
+        }
+    }
+
+    public void leaveGame(String authToken, int gameID) throws ResponseException {
+        try{
+            UserGameCommand leaveCommand = new UserGameCommand(UserGameCommand.CommandType.LEAVE,authToken,gameID);
+            sesh.getBasicRemote().sendText(new Gson().toJson(leaveCommand));
+        } catch (IOException e) {
+            throw new ResponseException(e.getMessage());
+        }
+    }
+
+    public void resignGame(String authToken, int gameID) throws ResponseException {
+        try{
+            UserGameCommand resignCommand = new UserGameCommand(UserGameCommand.CommandType.RESIGN,authToken,gameID);
+            sesh.getBasicRemote().sendText(new Gson().toJson(resignCommand));
+        } catch (IOException e) {
+            throw new ResponseException(e.getMessage());
+        }
+    }
+
+}
